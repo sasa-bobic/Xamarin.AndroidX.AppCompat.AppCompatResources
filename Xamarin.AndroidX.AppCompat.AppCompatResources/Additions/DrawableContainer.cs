a68f04d9779628860e86005ab9c44879264a578e
Saša Bobić <sasa.bobic@vipdata.hr>
2023-03-09 13:15:17 +0100

    Add project files.

---
+++ Xamarin.AndroidX.AppCompat.AppCompatResources/Additions/DrawableContainer.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AndroidX.AppCompat.Graphics.Drawable
+{
+    public class DrawableContainer : DrawableContainerCompat
+    {
+    }
+}
---
+++ Xamarin.AndroidX.AppCompat.AppCompatResources/Additions/DrawableWrapper.cs
@@ -0,0 +1,15 @@
+﻿using Android.Runtime;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AndroidX.AppCompat.Graphics.Drawable
+{
+    public class DrawableWrapper : DrawableWrapperCompat
+    {
+        public DrawableWrapper(global::Android.Graphics.Drawables.Drawable drawable) : base(drawable)
+        {
+
+        }
+    }
+}
